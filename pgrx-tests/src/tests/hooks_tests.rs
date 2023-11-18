2744abe7e94c5d8fe1ea419c4d5b43949a60542f
Tom Forbes <tom@tomforb.es>
2023-11-18 19:34:26 +0000

    Add a repo for logging issues

--- pgrx-tests/src/tests/hooks_tests.rs
+++ pgrx-tests/src/tests/hooks_tests.rs
@@ -138,6 +138,7 @@ mod tests {
                     arg: *mut ::std::os::raw::c_void,
                 ) -> HookResult<()>,
             ) -> HookResult<()> {
+                log!("{access} - {class_id} - {object_id} - {sub_id}!");
                 self.accesses += 1;
                 prev_hook(access, class_id, object_id, sub_id, arg)
             }
@@ -155,7 +156,7 @@ mod tests {
 
         Spi::run("ALTER table test_hooks_table add column baz boolean").expect("SPI failed");
         // This is 3 because there are two accesses for namespaces, and one for the table itself.
-        assert_eq!(3, HOOK.accesses);
+        assert_eq!(4, HOOK.accesses);
 
         // TODO:  it'd be nice to also test that .commit() and .abort() also get called
         //    but I don't see how to do that since we're running *inside* a transaction here
